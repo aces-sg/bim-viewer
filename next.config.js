5c7e6004c478b0567db5f4740a55bcb957fddb6c
tangytang <ivan@aceplp.com.sg>
2023-10-03 16:24:10 +0800

    fix ES6 comptability issues

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ['openbim-components', '@popperjs', 'bim-fragment', 'openbim-clay'],
 }
 
 module.exports = nextConfig
