0cadcfc58c2bd15334750cb37f28df5f7d4f3f74
tangytang <ivan@aceplp.com.sg>
2023-10-13 10:30:39 +0800

    Bug: ui errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,50 +1,50 @@
-import {Inter} from 'next/font/google'
-import Viewer from '@/components/Viewer';
-import Sidebar from '@/components/Sidebar';
+import { Inter } from "next/font/google";
+import Viewer from "@/components/Viewer";
+import Sidebar from "@/components/Sidebar";
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({ subsets: ["latin"] });
 
 interface tHomeProps {
-    posts: number[],
-    formattedDate: string
+  posts: number[];
+  formattedDate: string;
 }
 
 export default function Home(props: tHomeProps) {
-    const {formattedDate} = props;
-    return (
-        <div 
-        id="app" 
-        style={{
-            width: "100vw",
-            height: "100vh",
-            display: "grid",
-            gridTemplateColumns: "60px 1fr",
-            gridTemplateAreas: "'sidebar viewer'",
-        }}
-        >
-            <Sidebar />
-            <Viewer />
-        </div>
-    );
+  const { formattedDate } = props;
+  return (
+    <div
+      id="app"
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "grid",
+        gridTemplateColumns: "60px 1fr",
+        gridTemplateAreas: "'sidebar viewer'",
+      }}
+    >
+      <Sidebar />
+      <Viewer />
+    </div>
+  );
 }
 
-export async function getStaticProps() {
-    const buildDate = Date.now();
-    try {
-        const formattedDate = new Intl.DateTimeFormat("en-US", {
-            dateStyle: "long",
-            timeStyle: "long",
-        }).format(buildDate);
+// export async function getStaticProps() {
+//     const buildDate = Date.now();
+//     try {
+//         const formattedDate = new Intl.DateTimeFormat("en-US", {
+//             dateStyle: "long",
+//             timeStyle: "long",
+//         }).format(buildDate);
 
-        const res = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty');
-        const postIds: number[] = await res.json();
-        return {
-            props: {
-                posts: postIds,
-                formattedDate
-            }
-        };
-    } catch (e) {
-        console.log(e);
-    }
-}
+//         const res = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty');
+//         const postIds: number[] = await res.json();
+//         return {
+//             props: {
+//                 posts: postIds,
+//                 formattedDate
+//             }
+//         };
+//     } catch (e) {
+//         console.log(e);
+//     }
+// }
