5f9218662f3311cf52a6417b11d0a07ba8c25509
tptspe <tranphuoctri96@gmail.com>
2023-11-03 18:19:58 +0700

    update the sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,30 +1,18 @@
 import React from "react";
 import * as OBC from "openbim-components";
-export default function Sidebar() {
-  const sidebarStyle: React.CSSProperties = {
-    gridArea: "sidebar",
-    backgroundColor: "#202932",
-    borderRightColor: "#3c4854",
-    borderRightWidth: "1px",
-    display: "flex",
-    flexDirection: "column",
-    rowGap: "12px",
-    justifyContent: "center",
-    alignItems: "center",
-  };
-
+const Sidebar = () => {
   const links = {
     home: "https://www.bim.com.sg",
     projects: "/projects",
   };
 
   return (
-    <>
-      <div style={sidebarStyle}>
-        <a href={links.home}>
-          <span className={OBC.Button.Class.Base + "material-icons"}>home</span>
-        </a>
-      </div>
-    </>
+    <div>
+      <a href={links.home}>
+        <span className={OBC.Button.Class.Base + "material-icons"}>home</span>
+      </a>
+    </div>
   );
-}
+};
+
+export default Sidebar;
