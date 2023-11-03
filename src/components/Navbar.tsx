b3e069fd21b33f00c7f9bf9641da5e868f62ca26
tptspe <tranphuoctri96@gmail.com>
2023-11-04 00:29:19 +0700

    update the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,12 +4,16 @@ import LeftArrowIcon from "./icons/LeftArrowIcon";
 
 const Navbar: FC = () => {
   return (
-    <div className="bg-white px-[40px] py-[17.5px]">
-      <Link href="https://www.bim.com.sg">
+    <div className="bg-white px-[40px] py-[8px] flex items-center justify-between">
+      <Link href="https://www.bim.com.sg" className="flex items-center">
         <LeftArrowIcon />
-        <span>Back To Homepage</span>
+        <span className="ml-2 font-sans font-normal text-[14px] leading-[21px] text-[#666]">
+          Back To Homepage
+        </span>
       </Link>
-      <div className=""></div>
+      <div className="bg-[#fddb00] w-[40px] h-[40px] flex items-center justify-center rounded-full font-sans font-semibold text-[14px] leading-[21px] text-[#000] cursor-pointer">
+        AN
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,11 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    fontFamily: {
+      sans: [
+        '"Poppins", sans-serif',
+      ],
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
