0a8af1bb73bc75de8cc5ccefa69d089438912d7e
tptspe <tranphuoctri96@gmail.com>
2023-11-05 02:10:24 +0700

    add the menus

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,75 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
+
+import { useOutsideClick } from "@/hooks/useOutsideClick";
+
 import LeftArrowIcon from "./icons/LeftArrowIcon";
 
 const Navbar: FC = () => {
+  const menus = [
+    { id: 1, name: "Account", iconWidth: 13.5, iconLink: "userAccountIcon" },
+    { id: 2, name: "Logout", iconWidth: 15, iconLink: "logoutIcon" },
+  ];
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleShowMenu = () => {
+    setShowMenu(true);
+  };
+
+  const handleMenuOption = (menuOptionId: number) => {
+    if (menuOptionId === 1) {
+      console.log("handle account");
+    } else {
+      console.log("handle logout");
+    }
+    setShowMenu(false);
+  };
+
+  const menuRef = useOutsideClick(() => {
+    setShowMenu(false);
+  });
+
   return (
-    <div className="bg-white px-[40px] py-[8px] flex items-center justify-between border-b-[1px] border-solid border-[#d2d2d2]">
+    <div className="bg-white px-[40px] py-[8px] flex items-center justify-between border-b-[1px] border-solid border-[#d2d2d2] relative">
       <Link href="https://www.bim.com.sg" className="flex items-center">
         <LeftArrowIcon />
         <span className="ml-2 font-sans font-normal text-[14px] leading-[21px] text-[#666]">
           Back To Homepage
         </span>
       </Link>
-      <div className="bg-[#fddb00] w-[40px] h-[40px] flex items-center justify-center rounded-full font-sans font-semibold text-[14px] leading-[21px] text-[#000] cursor-pointer">
+      <div
+        className="bg-[#fddb00] w-[40px] h-[40px] flex items-center justify-center rounded-full font-sans font-semibold text-[14px] leading-[21px] text-[#000] cursor-pointer"
+        onClick={handleShowMenu}
+      >
         AN
       </div>
+      {showMenu && (
+        <div
+          className="bg-white absolute w-[200px] top-[60px] right-[40px] z-50 px-[16px] py-[8px] rounded-[8px]"
+          ref={menuRef}
+        >
+          {menus.map((menu, index) => (
+            <div
+              className="flex items-center h-[37px] cursor-pointer"
+              key={index}
+              onClick={() => handleMenuOption(menu.id)}
+            >
+              <Image
+                priority
+                src={`/images/${menu.iconLink}.svg`}
+                width={menu.iconWidth}
+                height={13.5}
+                alt={menu.iconLink}
+                className="cursor-pointer"
+              />
+              <span className="ml-2 font-sans font-normal text-[14px] leading-[21px] text-[#121212]">
+                {menu.name}
+              </span>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,5 +1,8 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import Image from "next/image";
+
+import { useOutsideClick } from "@/hooks/useOutsideClick";
+
 import { Project } from "@/types";
 
 interface ProjectCardProps {
@@ -7,20 +10,84 @@ interface ProjectCardProps {
 }
 
 const ProjectCard: FC<ProjectCardProps> = ({ project }) => {
+  const menus = [
+    {
+      id: 1,
+      name: "Invite",
+      iconWidth: 18.3,
+      iconHeight: 13.3,
+      iconLink: "inviteIcon",
+    },
+    {
+      id: 2,
+      name: "Share",
+      iconWidth: 15,
+      iconHeight: 16.7,
+      iconLink: "shareIcon",
+    },
+  ];
+
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleMenuOption = (menuOptionId: number) => {
+    if (menuOptionId === 1) {
+      console.log("handle invite");
+    } else {
+      console.log("handle share");
+    }
+    setShowMenu(false);
+  };
+
+  const handleShowMenu = () => {
+    setShowMenu(true);
+  };
+
+  const menuRef = useOutsideClick(() => {
+    setShowMenu(false);
+  });
+
   return (
     <div className="bg-white rounded-[8px] p-[20px] border-[1px] border-solid border-[#aaa]">
-      <div className="flex items-start justify-between mb-[16px]">
+      <div className="flex items-start justify-between mb-[16px] relative">
         <h5 className="font-sans font-semibold text-[16px] leading-[24px] text-[#000] capitalize">
           {project.title}
         </h5>
-        <Image
-          priority
-          src="/images/threeDot.svg"
-          width={4}
-          height={16}
-          alt="threeDotIcon"
-          className="cursor-pointer ml-10"
-        />
+        <div onClick={handleShowMenu}>
+          <Image
+            priority
+            src="/images/threeDot.svg"
+            width={4}
+            height={16}
+            alt="threeDotIcon"
+            className="cursor-pointer ml-10"
+          />
+        </div>
+        {showMenu && (
+          <div
+            className="bg-white absolute w-[200px] top-[25px] right-[0px] z-50 px-[16px] py-[8px] rounded-[8px] shadow-[0px_4px_8px_4px_rgba(0,0,0,0.1)]"
+            ref={menuRef}
+          >
+            {menus.map((menu, index) => (
+              <div
+                className="flex items-center h-[37px] cursor-pointer"
+                key={index}
+                onClick={() => handleMenuOption(menu.id)}
+              >
+                <Image
+                  priority
+                  src={`/images/${menu.iconLink}.svg`}
+                  width={menu.iconWidth}
+                  height={menu.iconHeight}
+                  alt={menu.iconLink}
+                  className="cursor-pointer"
+                />
+                <span className="ml-2 font-sans font-normal text-[14px] leading-[21px] text-[#121212]">
+                  {menu.name}
+                </span>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
       <div className="flex">
         <p className="font-sans font-normal text-[12px] leading-[18px] text-[#666] capitalize">
