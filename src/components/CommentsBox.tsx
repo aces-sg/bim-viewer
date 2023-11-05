4a717ae4f06305f29a35c18655d58dc6e75e03b9
tptspe <tranphuoctri96@gmail.com>
2023-11-06 02:43:29 +0700

    sync change

--- src/components/CommentsBox.tsx
+++ src/components/CommentsBox.tsx
@@ -29,11 +29,25 @@ const comments: Comment[] = [
     description:
       "Lorem ipsum dolor sit amet consectetur. Sit sagittis lorem pellentesque sed. Eget elit nunc accumsan tempor. Augue id sit eleifend fermentum in aenean.",
   },
+  {
+    id: 4,
+    name: "Francisca Lorraine",
+    time: "5 mins ago",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Sit sagittis lorem pellentesque sed. Eget elit nunc accumsan tempor. Augue id sit eleifend fermentum in aenean.",
+  },
+  {
+    id: 5,
+    name: "James Arthur",
+    time: "5 mins ago",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Sit sagittis lorem pellentesque sed. Eget elit nunc accumsan tempor. Augue id sit eleifend fermentum in aenean.",
+  },
 ];
 
 const CommentsBox: FC<CommentsBoxProps> = ({ closeBox }) => {
   return (
-    <div className="fixed top-0 right-0 sm:w-full md:w-[500px] lg:w-[500px] h-full shadow-[-4px_0px_8px_0px_rgba(0,0,0,0.1)] bg-[#f2f2f2] z-40">
+    <div className="fixed top-0 right-0 sm:w-full md:w-[500px] lg:w-[500px] h-full shadow-[-4px_0px_8px_0px_rgba(0,0,0,0.1)] bg-[#f2f2f2] z-40 overflow-y-scroll">
       <div className="bg-white px-[20px] py-[16px] flex items-center justify-between">
         <h5 className="font-sans font-semibold text-[20px] leading-[30px] text-[#000]">
           Comments
@@ -48,12 +62,12 @@ const CommentsBox: FC<CommentsBoxProps> = ({ closeBox }) => {
           onClick={closeBox}
         />
       </div>
-      <div className="p-[20px]">
+      <div className="px-[20px] pt-[20px] pb-[80px]">
         {comments.map((comment, index) => (
           <CommentCard key={index} comment={comment} />
         ))}
       </div>
-      <div className="flex items-center bg-white px-[20px] py-[16px]">
+      <div className="fixed w-[490px] bottom-0 flex items-center bg-white px-[20px] py-[16px]">
         <input
           type="text"
           className="rounded-[8px] p-[10px] border-[1px] border-solid border-[#aaa] w-full h-[38px]"
