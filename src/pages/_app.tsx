3258d912cbce1f343ddb009a6003b466d4a05917
tangytang <ivan@aceplp.com.sg>
2023-11-09 14:36:14 +0800

    configure amplify

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,22 @@
 // import "@/styles/globals.css";
 import "@/styles/app.css";
+import { Amplify, Auth } from "aws-amplify";
 import type { AppProps } from "next/app";
+import config from "@/aws-exports";
+
+Amplify.configure({
+  ...config,
+  ssr: true,
+});
 
 import Layout from "@/components/layout";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    let res = Auth.currentAuthenticatedUser();
+    console.log("user is", res);
+  });
   return (
     <Layout>
       <Component {...pageProps} />
