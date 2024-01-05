efcda8564d9fe577206f241ed06e35973c4a6819
tangytang <ivan@aceplp.com.sg>
2024-01-05 10:05:51 +0800

    Feature: Added SEO component

---
+++ next-seo.config.js
@@ -0,0 +1,10 @@
+export default {
+  title: "Bimeco Viewer",
+  description: "The free online IFC viewer with no installation needed.",
+  openGraph: {
+    type: "website",
+    locale: "en_IE",
+    url: "https://viewer.com.com.sg",
+    siteName: "Bimeco Viewer",
+  },
+};
